29263de6b1482fb8a233b014ee7c821c26325ed8
Nabs <orientnab@gmail.com>
2020-05-12 18:51:30 +0200

    [ADD] Encapsulate input information in one object
    The programInput object contains the input/output filenames and
    algorithm to be used.
    It also contains an enum with the name of the algorithms in case to be
    extended.

---
+++ src/program_input.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <map>
+#include <string>
+
+struct programInput {
+
+  enum Algos {
+    dijkstra = 0,
+    astar = 1
+  };
+
+  std::map<std::string, Algos> algorithms = {
+    {"dijkstra", Algos::dijkstra},
+    {"astar", Algos::astar}
+  };
+
+  bool input_from_stdin = false;
+  bool output_to_stdout = true;
+  std::string inputFile;
+  std::string outputFile;
+  Algos algorithm = Algos::dijkstra;
+
+};
+
